1e942564053062fd94c862fcb806dbc66e6addfa
Cotelea Carlo <coteleacarlo@gmail.com>
2021-04-06 16:15:53 +0300

    made a cool ass user system

--- main.cpp
+++ main.cpp
@@ -3,10 +3,23 @@
 
 using namespace std;
 
+struct person{
+    char name[120], gender;
+    int age;
+};
+
+void makeUser(person &p) {
+    cout << "Greetings! What is your name?" << endl;
+    cin.clear();
+    cin.getline(p.name, 120);
+    cout << "Now please tell me your age and gender: ";
+    cin >> p.age >> p.gender;
+}
+
 int main() {
-    char name[256];
-    cin.getline(name, 256);
-    cout << "Hello, " << name;
+    person p;
+    makeUser(p);
+    cout << "Hello, " << p.name;
 
     return 0;
 }
