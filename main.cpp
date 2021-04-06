c9427ada0a974238d72525fa8f1595e4f336bcfa
Cotelea Carlo <coteleacarlo@gmail.com>
2021-04-06 15:47:38 +0300

    added cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    char name[256];
+    cin.getline(name, 256);
+    cout << "Hello, " << name;
+
+    return 0;
+}
