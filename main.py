ade751faeb5734c866c95da826b872b291fdb064
Cotelea Carlo <coteleacarlo@gmail.com>
2021-04-06 16:26:19 +0300

    added main to py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("Hello World!")
-for i in range(1, 10):
-    print("current number is ", i)
+def main():
+    print("Hello World!")
+    for i in range(1, 10):
+        print("current number is ", i)
