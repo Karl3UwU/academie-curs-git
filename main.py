e072d677680f48deadd408529a1aa75e71c75eb8
Cotelea Carlo <coteleacarlo@gmail.com>
2021-04-06 16:43:39 +0300

    modified print

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
-    print("Hello World!")
+    print("Hello world!")
     for i in range(1, 10):
         print("current number is ", i)
+    print("that's pretty pog, innit?")
 
 if __name__ == '__main__':
     main()
